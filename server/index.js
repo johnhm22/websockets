ac62b0deb1280d39f51d4090a9f9638e12fd58b7
john <johnhowardm@hotmail.com>
2024-06-25 15:47:44 +0100

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-import { createServer } from 'http';
+import express from 'express';
 import { Server } from 'socket.io';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const httpServer = createServer();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === 'production'
@@ -24,4 +37,4 @@ io.on('connection', (socket) => {
   });
 });
 
-httpServer.listen(3500, () => console.log('listening on port 3500'));
+// httpServer.listen(3500, () => console.log('listening on port 3500'));
